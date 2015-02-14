36481b1c453ebb2e859a0cfa5ea4f07a6e911eb3
Dante Elrik <dante@cloudshopper.com>
2015-02-14 15:31:18 -0500

    reconsturcting a network

--- Main.hs
+++ Main.hs
@@ -4,13 +4,36 @@ module Data.BinaryTree.Main where
 
 data Tree a  = Node a | Branch (Tree a) (Tree a)
 
-showTree               :: Show a =>Tree a -> String
-showTree (Node x)      = show x
-showTree (Branch l r)  = "<" ++ showTree l ++ "|" ++ showTree r ++ ">"
+type Network a = [Tree a]
+
+xshowTree               :: Show a =>Tree a -> String
+xshowTree (Node x)      = show x
+xshowTree (Branch l r)  = "<" ++ showTree l ++ "|" ++ showTree r ++ ">"
 
 showsTree              :: Show a => Tree a -> ShowS
 showsTree (Node x)     = shows x
 showsTree (Branch l r) = ('<':) . showsTree l . ('|':) . showsTree r . ('>':)
+
+showTree t = showsTree t ""
+
+readsTree          :: Read a => ReadS (Tree a)
+readsTree ('<':s)  = do
+  (l, '|':ls) <- readsTree s
+  (r, '>':u)  <- readsTree ls
+  return (Branch l r,u)
+readsTree s = do
+  (x, t) <- reads s
+  return (Node x,t)
+
+readsTrees :: String -> Network a
+readsTrees ""          = []
+readsTrees enc'Network = do
+  tree <- readsTree enc'tree
+  fst tree : readsTrees enc'trees
+  where
+    (enc'tree:enc'trees) = lines enc'Network
+
+
 
 t :: Tree Int
 t = Branch
@@ -21,3 +44,6 @@ t = Branch
       (Node 6)
      )
     )
+t'enc  = showTree t
+t'dec :: [(Tree Int, String)]
+t'dec  = readsTree t'enc
